ec7e5dc84ac5bcbea38f1686fcc9a1693fb8e0e7
carson <jacobs.car3376@stu.stech.edu>
2019-02-22 09:45:04 -0700

    added Ch. 7
    added Ch. 7

---
+++ Ch. 7/Ch_7_27.java
@@ -0,0 +1,28 @@
+/*
+ Author: Carson Jacobs
+ Date: 2/20/2019
+ Locker Puzzle
+ */
+
+
+
+import java.util.Scanner;
+public class Ch_7_27 {
+
+	public static void main(String[] args) {
+		boolean[] lockers = new boolean[100];
+		
+		for (int student = 1; student <= 100; student++) {
+			for(int a = (student-1); a < 100; a += student) {
+				lockers[a] = !lockers[a];
+			
+			}
+		}
+	
+			if(lockers[i] == true) {
+				System.out.printf("L%d ", i + 1);
+			
+			}
+		}
+	}
+}
